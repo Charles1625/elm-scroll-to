a4faea270d025b8f7201b2152ac671a4e99aa449
Charles Looker <58594763+Charles1625@users.noreply.github.com>
2020-07-16 20:28:26 +0100

    Use on window and/or one or more elements
    Update on the for-element branch which could be used for a specific element, but not for more than one.

--- src/ScrollTo.elm
+++ src/ScrollTo.elm
@@ -30,6 +30,7 @@ import Browser.Dom
 import Browser.Events
 import ScrollTo.Animation as Animation
 import Task
+import Debug
 
 
 {-| Represent the state of the scroll; the status will remain waiting until a
@@ -39,14 +40,28 @@ scroll command is issued.
         { scrollToStatus : ScrollTo.Status }}
 
 -}
+<<<<<<< Updated upstream
 type Status
     = Waiting Animation.Animation
+=======
+type Status msg
+    = Waiting 
+        { animation : Animation.Animation
+        , target : Target
+        , usrMsg : Msg -> msg
+        }
+>>>>>>> Stashed changes
     | Animating
         { scene : Dimensions
         , to : Position
         , from : Position
         , animation : Animation.Animation
         , elapsed : Float
+<<<<<<< Updated upstream
+=======
+        , target : Target
+        , usrMsg : Msg -> msg
+>>>>>>> Stashed changes
         }
 
 
@@ -67,9 +82,34 @@ type alias Position =
     ScrollTo.init
 
 -}
+<<<<<<< Updated upstream
 init : Status
 init =
     Waiting Animation.init
+=======
+init : ( Msg -> msg ) -> Status msg
+init usrMsg =
+    Waiting 
+        { animation = Animation.init
+        , target = Window
+        , usrMsg = usrMsg
+        }
+
+
+{-| Setup a scroll command for a specific scrollable element 
+    (not the whole window)
+
+    ScrollTo.initFor "element-id"
+
+-}
+initFor : ( Msg -> msg ) -> String -> Status msg
+initFor usrMsg id =
+    Waiting
+        { animation = Animation.init
+        , target = Element id
+        , usrMsg = usrMsg
+        }
+>>>>>>> Stashed changes
 
 
 {-| Add a delay (in ms) to your scroll command.
@@ -78,7 +118,7 @@ init =
     ScrollTo.withDelay 1000
 
 -}
-withDelay : Float -> Status -> Status
+withDelay : Float -> Status msg -> Status msg
 withDelay delay scroll =
     case scroll of
         Waiting animation ->
@@ -94,7 +134,7 @@ withDelay delay scroll =
     ScrollTo.withDuration 5000
 
 -}
-withDuration : Float -> Status -> Status
+withDuration : Float -> Status msg -> Status msg
 withDuration duration scroll =
     case scroll of
         Waiting animation ->
@@ -112,7 +152,7 @@ to your scroll command.
     ScrollTo.withEasing Ease.inOutQuint
 
 -}
-withEasing : (Float -> Float) -> Status -> Status
+withEasing : (Float -> Float) -> Status msg -> Status msg
 withEasing easing scroll =
     case scroll of
         Waiting animation ->
@@ -137,9 +177,31 @@ withEasing easing scroll =
     ScrollTo.toPosition { x = 1080, y = 540 }
 
 -}
+<<<<<<< Updated upstream
 toPosition : Position -> Cmd Msg
 toPosition to =
     Task.perform (GotViewport to) Browser.Dom.getViewport
+=======
+toPosition : Position -> Status msg -> Cmd Msg
+toPosition to scroll =
+    let 
+        tgt = {-for either whole window of scrollable elment-}
+            case scroll of
+                Waiting { target } ->
+                    target
+                Animating { target } ->
+                    target
+    in
+        let 
+            task = 
+                case tgt of 
+                    Window ->
+                        Browser.Dom.getViewport
+                    Element id ->
+                        Browser.Dom.getViewportOf id
+        in
+            Task.attempt (GotViewport to) task
+>>>>>>> Stashed changes
 
 
 {-| Track scroll messages.
@@ -157,12 +219,13 @@ type Msg
 
 {-| Handle updates from the scroll animation.
 
-    ScrollTo.update scrollToMsg model.scrollToStatus
+    ScrollTo.update scrollToMsg model.scrollToStatus msg
 
 -}
-update : Msg -> Status -> ( Status, Cmd Msg )
+update : Msg -> Status msg -> ( Status msg, Cmd msg )
 update msg scroll =
     case msg of
+<<<<<<< Updated upstream
         GotViewport to viewport ->
             case scroll of
                 Waiting animation ->
@@ -178,30 +241,73 @@ update msg scroll =
         StartAnimation to { scene, viewport } ->
             case scroll of
                 Waiting animation ->
+=======
+        GotViewport to result ->
+            case result of
+                Ok viewport ->
+                    case scroll of
+                        Waiting { animation, usrMsg } ->
+                            ( scroll
+                            , Animation.wait animation <| 
+                                usrMsg <|
+                                    StartAnimation to viewport
+                            )
+
+                        Animating { animation, usrMsg } ->
+                            ( scroll
+                            , Animation.wait animation <| 
+                                usrMsg <|
+                                    StartAnimation to viewport
+                            )
+                Err _ ->
+                    ( scroll, Cmd.none)
+
+        StartAnimation to { scene, viewport } ->
+            case scroll of
+                Waiting { animation, target, usrMsg } ->
+>>>>>>> Stashed changes
                     ( Animating
                         { scene = { height = scene.height, width = scene.width }
                         , to = to
                         , from = { x = viewport.x, y = viewport.y }
                         , animation = animation
                         , elapsed = 0
+<<<<<<< Updated upstream
+=======
+                        , target = target
+                        , usrMsg = usrMsg
+>>>>>>> Stashed changes
                         }
                     , Cmd.none
                     )
 
+<<<<<<< Updated upstream
                 Animating { animation } ->
+=======
+                Animating { animation, target, usrMsg } ->
+>>>>>>> Stashed changes
                     ( Animating
                         { scene = { height = scene.height, width = scene.width }
                         , to = to
                         , from = { x = viewport.x, y = viewport.y }
                         , animation = animation
                         , elapsed = 0
+<<<<<<< Updated upstream
+=======
+                        , target = target
+                        , usrMsg = usrMsg
+>>>>>>> Stashed changes
                         }
                     , Cmd.none
                     )
 
         Step delta ->
             case scroll of
+<<<<<<< Updated upstream
                 Animating ({ to, from, animation, elapsed, scene } as data) ->
+=======
+                Animating ({ to, from, animation, elapsed, scene, target, usrMsg } as data) ->
+>>>>>>> Stashed changes
                     let
                         time =
                             delta + elapsed
@@ -210,15 +316,26 @@ update msg scroll =
                             Animation.step time animation
                     in
                     if Animation.isFinished animation time then
+<<<<<<< Updated upstream
                         ( Waiting animation
                         , Task.perform (\_ -> NoOp) <|
                             Browser.Dom.setViewport to.x to.y
+=======
+                        ( Waiting { animation = animation, target = target, usrMsg = usrMsg }
+                        , Task.attempt (\_ -> usrMsg NoOp) <|
+                            task to.x to.y
+>>>>>>> Stashed changes
                         )
 
                     else
                         ( Animating { data | elapsed = time }
+<<<<<<< Updated upstream
                         , Task.perform (\_ -> NoOp) <|
                             Browser.Dom.setViewport
+=======
+                        , Task.attempt (\_ -> usrMsg NoOp) <|
+                            task
+>>>>>>> Stashed changes
                                 (clamp 0
                                     scene.width
                                     (from.x + (to.x - from.x) * step)
@@ -241,11 +358,11 @@ update msg scroll =
     ScrollTo.subscriptions ScrollToMsg model.scrollToStatus
 
 -}
-subscriptions : (Msg -> msg) -> Status -> Sub msg
-subscriptions msg scroll =
+subscriptions : Status msg -> Sub msg
+subscriptions scroll =
     case scroll of
-        Animating _ ->
-            Browser.Events.onAnimationFrameDelta (\delta -> msg <| Step delta)
+        Animating { usrMsg } ->
+            Browser.Events.onAnimationFrameDelta (\delta -> usrMsg <| Step delta)
 
         _ ->
             Sub.none
